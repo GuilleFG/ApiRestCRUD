fe0827a29031875bdd55ef6bf470916bce2d0cf5
Guillermo Fernández <guillefergom@gmail.com>
2024-05-16 15:32:36 +0200

    custom error messages

--- src/main/java/com/guillermo/curso/springboot/app/springbootcrud/SpringbootCrudApplication.java
+++ src/main/java/com/guillermo/curso/springboot/app/springbootcrud/SpringbootCrudApplication.java
@@ -2,8 +2,10 @@ package com.guillermo.curso.springboot.app.springbootcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+
 public class SpringbootCrudApplication {
 
     public static void main(String[] args) {
